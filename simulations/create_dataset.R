0b4c8baecc51d7082e3a315f6322e6747e349e50
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-01-21 15:18:24 -0500

    update

--- simulations/create_dataset.R
+++ simulations/create_dataset.R
@@ -4,12 +4,17 @@ library(getopt)
 
 spec <- matrix(c(
   'fit',        'i', 1, "character",
-  'cluster_id', 'c', 1, "character",
+  'data',       'd', 1, "character",
+  'subject',    'u', 1, "character",
   'logFC',      'l', 1, "numeric",
   'pDE',        'p', 1, "numeric",
   'libScaleFactor', 's', 1, "numeric",
+  'seed',       'e', 1, "integer",
   'output',     'o', 1, "character"
 ), byrow=TRUE, ncol=4)
+
+# Parse command line arguments
+opt <- getopt(spec)
 
 library(lucida)
 library(SingleCellExperiment)
@@ -17,12 +22,23 @@ library(tidyverse)
 
 # load lucida model
 fit <- readRDS( opt$fit )
+data <- readRDS( opt$data )
+
+# set seed
+set.seed(opt$seed)
+
+# Set disease status for simulations
+data$Dx = "Control"
+Dx_lvls = sample(levels(data[[opt$subject]]), nlevels(data[[opt$subject]])/2)
+data$Dx[data[[opt$subject]] %in% Dx_lvls] = "Disease"
+
+formula = as.formula(paste("~(1|", opt$subject, ")"))
 
 # Simulate based on fit
 sce.sim <- simulateCountData(
   fit = fit, 
-  formula = ~ (1|Individual), 
-  data = colData(sce), 
+  formula = formula, 
+  data = data, 
   target = "Dx", 
   logFC = opt$logFC, 
   pDE = opt$pDE, 
