b8b77545f0f9c6d51641b8f2496539b10b2560f4
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-01-21 22:19:50 -0500

    update

--- simulations/de_methods.R
+++ simulations/de_methods.R
@@ -228,6 +228,8 @@ run_analysis <- function( sce.sim, formula, cluster_id, methods){
     df <- bind_rows(df, res.gp)
   }
 
+  browser()
+  
   df <- df %>% 
     inner_join(metadata(sce.sim), by=c(cluster_id, "ID")) %>%
     mutate(Method = factor(Method, validMethods)) %>%
