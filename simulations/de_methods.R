e862e6e17166bd6f20feedba0985371681be4495
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-03-05 17:14:44 -0500

    add DESeq2

--- simulations/de_methods.R
+++ simulations/de_methods.R
@@ -389,7 +389,7 @@ run_analysis <- function( sce.sim, formula, cluster_id, methods, nthreads = 1){
     df <- bind_rows(df, res.gp)
   }
 
-  if( "info" %in% names(metadata(sce3)) ){
+  if( "info" %in% names(metadata(sce.sim)) ){
 
     df <- df %>% 
       inner_join(metadata(sce.sim) %>%
