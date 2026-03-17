e76b08ceded713227c4515c347a861dbe47b5d2a
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-03-17 11:56:35 -0400

    update

--- simulations/de_methods.R
+++ simulations/de_methods.R
@@ -256,6 +256,7 @@ run_analysis <- function( sce.sim, formula, coefTest, cluster_id, methods, nthre
           P.Value = "p_val", 
           FDR = 'p_adj.glb')
 
+      browser()
       if( method == "DESeq2"){
         tab = tab %>%
           dplyr::select(-baseMean, -lfcSE, -stat, -p_adj.loc)
