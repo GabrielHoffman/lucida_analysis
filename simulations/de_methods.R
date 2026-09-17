3931a52bde556ef9813f2a4583be596429d05c99
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2026-09-17 17:14:20 -0400

    update

--- simulations/de_methods.R
+++ simulations/de_methods.R
@@ -330,7 +330,7 @@ run_analysis <- function( sce.sim, formula, coefTest, cluster_id, methods, nthre
           method = method, 
           design = design, 
           coef = which(coefTest == colnames(design)), 
-          min_cells = 2, 
+          min_cells = 1, 
           filter = "none")}, 
           error = function(e) success <<- FALSE)
       })
